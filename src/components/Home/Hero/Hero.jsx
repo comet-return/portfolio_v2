e3a65cb1008cd358c24871fe1752cdce4533021b
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-06-20 21:50:58 +0530

    animation added

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -2,11 +2,32 @@ import Button from "../../Button/Button.jsx";
 import SocialButton from "../../Button/SocialButton.jsx";
 import {socialNavs} from "../../../utils/HeroConstants.jsx";
 import "./Hero.css"
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 const Hero = () => {
+
+  useGSAP(() => {
+    gsap.from(".headline > *", {
+      duration: 1,
+      opacity: 0,
+      x: -100,
+      ease: "power3.out",
+      stagger: 0.2
+    });
+    gsap.from(".hero-img", {
+      duration: 1.5,
+      opacity: 0,
+      x: 200,
+      ease: "power3.out",
+    });
+  });
+
+
   return (
     <section className="hero h-full w-full flex flex-col md:flex-row items-center justify-between md:p-20 p-16 md:px-44">
         <div className="headline">
-            <h1 className="text-7xl mb-1">HI, I AM <br />MOHAMMAD ZAID</h1>
+            <h1 className="text-7xl">HI, I AM</h1>
+            <h1 className="text-7xl mb-1">MOHAMMAD ZAID</h1>
             <p className="mb-5">Full stack developer with a focus on web applications</p>
             <div className="headline-navs flex items-center gap-4">
               <Button text={"CONTACT ME"}/>
@@ -23,7 +44,7 @@ const Hero = () => {
         </div>
 
         <div className="hero-img bg-slate-700">
-          <img src="" alt="My_Image" />
+          <img src="https://images-platform.99static.com/ky8wEq_DEdvfN0l2lmUSfuVYL0g=/0x0:2000x2000/500x500/top/smart/99designs-contests-attachments/107/107446/attachment_107446690" alt="My_Image" />
         </div>
 
     </section>
