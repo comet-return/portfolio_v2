e7bb0c1b4d6cb035a6c55d0163a3e2bc0fd116af
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-06-20 21:03:07 +0530

    added constants

---
+++ src/utils/HeroConstants.jsx
@@ -0,0 +1,7 @@
+import { TfiLinkedin } from "react-icons/tfi";
+import { BsGithub } from "react-icons/bs";
+
+export const socialNavs = [
+    {link: "https://www.linkedin.com/in/mohammad-zaid-khan-dev/", icon: <TfiLinkedin />},
+    {link: "https://github.com/MZaidKhan2413", icon: <BsGithub />}
+]
