99b72b27233ac80f2d59d4f6be6751bfffca6924
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-06-21 17:10:40 +0530

    Contact section added

---
+++ src/utils/ContactConstants.jsx
@@ -0,0 +1,10 @@
+import { TfiLinkedin } from "react-icons/tfi";
+import { BsGithub, BsTwitterX } from "react-icons/bs";
+import { IoLogoInstagram } from "react-icons/io5";
+
+export const socialHandles = [
+    {link: "https://www.linkedin.com/in/mohammad-zaid-khan-dev/", icon: <TfiLinkedin />},
+    {link: "https://github.com/MZaidKhan2413", icon: <BsGithub />},
+    {link: "https://x.com/mZaidk_", icon: <BsTwitterX />},
+    {link: "https://www.instagram.com/z.a.i.d__k/", icon: <IoLogoInstagram />},
+]
