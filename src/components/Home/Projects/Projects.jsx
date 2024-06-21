22aab63e9e4148052f159a8c36ea06fa8ee9622b
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-06-21 10:56:57 +0530

    Project Section added

---
+++ src/components/Home/Projects/Projects.jsx
@@ -0,0 +1,22 @@
+import "./Projects.css";
+import Project from "./Project";
+import { ProjectAnimation } from "../../../utils/Animation";
+
+const Projects = () => {
+  ProjectAnimation();
+  return (
+    <section className="projects-section min-h-screen py-16 px-8 sm:p-20 lg:px-44">
+      <h1 className="text-6xl text-center sm:text-start">Featured Projects</h1>
+      <p className="text-center sm:text-start">Here are some projects that showcase my passion & taste of development</p>
+      <div className="projects-container grid md:grid-cols-2 grid-cols-1 gap-6 py-10">
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+      </div>
+    </section>
+  )
+}
+
+export default Projects
