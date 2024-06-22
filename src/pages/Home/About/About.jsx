07641ae252fb6c5b1fabc45af1c8d113b3d6a802
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-06-22 10:40:08 +0530

    updated about section

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -1,12 +1,13 @@
 import { AboutAnimation } from "../../../utils/Animation";
 import "./About.css"
+import { Link } from "react-router-dom";
 
 const About = () => {
   AboutAnimation();
   return (
-    <section className="about flex justify-between">
+    <section className="about flex md:flex-row flex-col md:justify-between justify-center">
       <h1 className="text-8xl py-5">About Me</h1>
-      <div className="about-description w-1/2 py-5">
+      <div className="about-description md:w-1/2 w-full py-5">
         <h2 className="text-3xl">I am a Full Stack Developer.</h2>
         <h2 className="text-3xl mb-3">Has a software engineering background</h2>
         <p className="text-sm mb-5 sm:mb-10">
@@ -17,7 +18,7 @@ const About = () => {
           solutions and adapting to emerging technologies for optimal
           performance.
         </p>
-        <a href="/about" className="sm:text-sm text-xs underline underline-offset-8">MORE ABOUT ME</a>
+        <Link to="/about" className="sm:text-sm text-xs underline underline-offset-8">MORE ABOUT ME</Link>
       </div>
     </section>
   );
