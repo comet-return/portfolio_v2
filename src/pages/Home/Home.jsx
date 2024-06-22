4babbcf090b3674e61c9c4da181bc310f241d539
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-06-22 11:36:29 +0530

    scroll smoothness

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,8 +2,24 @@ import Hero from "./Hero/Hero";
 import Projects from "./Projects/Projects";
 import About from "./About/About";
 import Contact from "../../components/Contact/Contact";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 const Home = () => {
+  const location = useLocation();
+  useEffect(()=>{
+    if(location.hash === "") {
+      document.getElementById("hero").scrollIntoView({ behavior: "smooth" });
+    } else if(location.hash === "#projects") {
+      document.getElementById("projects").scrollIntoView({ behavior: "smooth" });
+    } else if(location.hash === "#about") {
+      document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+    } else if(location.hash === "#contact") {
+      document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+    } else {
+      window.scrollTo(0, 0);
+    }
+  }, [location])
   return (
     <>
         <Hero />
