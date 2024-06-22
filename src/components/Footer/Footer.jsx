d78e9f39a46f548fb45985094d5e6b345fd82ce1
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-06-22 10:02:41 +0530

    updated footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
+import "./Footer.css";
+
 const Footer = () => {
   return (
-    <footer className="flex flex-col items-center justify-center py-5 bg-slate-900">
+    <footer className="flex flex-col items-center justify-center py-5">
       <p>&copy; 2024 Mohammad Zaid Khan</p>
     </footer>
   )
