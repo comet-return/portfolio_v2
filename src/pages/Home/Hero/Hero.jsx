91474c279d68dc7427ff826493c6d0713162d222
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-06-22 10:35:34 +0530

    updated hero section

--- src/pages/Home/Hero/Hero.jsx
+++ src/pages/Home/Hero/Hero.jsx
@@ -6,12 +6,12 @@ import "./Hero.css";
 const Hero = () => {
   HeroAnimation();
   return (
-    <section className="hero h-full w-full flex flex-col md:flex-row items-center justify-between">
+    <section className="hero h-full w-full flex flex-col md:flex-row items-center md:justify-between justify-center gap-10">
         <div className="headline">
-            <h1>HI, I AM</h1>
-            <h1 className="mb-1">MOHAMMAD ZAID</h1>
-            <p className="mb-5 text-center sm:text-start">Full stack developer with a focus on web applications</p>
-            <div className="headline-navs flex items-center gap-4">
+            <h1 className="">HI, I AM</h1>
+            <h1 className="mb-5">MOHAMMAD <span className="name-animate">ZAID</span></h1>
+            <p className="mb-5 text-center sm:text-start">Full stack developer with a focus on web applications. <br /> Enthusiasm in learning new technologies.</p>
+            <div className="headline-navs flex items-center md:justify-start justify-center gap-4">
               <Button text={"CONTACT ME"}/>
 
               {
